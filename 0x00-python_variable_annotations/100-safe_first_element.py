310b9b5fcacd834cea9c7539db24bb2899ef854e
Youssef <youssefkaramedu@gmail.com>
2024-06-06 23:54:51 +0000

    Add file 100

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -6,9 +6,9 @@ from typing import Sequence, Union
 
 def safe_first_element(lst: Sequence) -> Union[None, Sequence]:
     '''
-    Function to return the first element of a sequence or None if the sequence is empty
+    Func to return the first elem of a seq or None
     '''
     if lst:
         return lst[0]
     else:
-        return None    return [(i, len(i)) for i in lst]
+        return None
