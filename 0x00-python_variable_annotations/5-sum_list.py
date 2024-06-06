ad325e9f5dcc71b11c939b0fb0076fd48ea12490
Youssef <youssefkaramedu@gmail.com>
2024-06-06 23:09:22 +0000

    Adhere to pycodestyle

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 ''' Task 5 module '''
 
-
 from typing import List
+
 
 def sum_list(input_list: List[float]) -> float:
     ''' Function to sum a list of floats and return the sum as a float '''
