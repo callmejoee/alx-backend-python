06ae57af7b0187a40db7277a79d6bf1b4830f426
Youssef <youssefkaramedu@gmail.com>
2024-06-06 23:08:07 +0000

    Add file 4

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,11 @@
 ''' Task 5 module '''
 
 
-def sum_list(input_list: list[float]) -> float:
-    ''' function to sum floats into one int '''
-    result = 0
+from typing import List
+
+def sum_list(input_list: List[float]) -> float:
+    ''' Function to sum a list of floats and return the sum as a float '''
+    result = 0.0  # Initialize result as a float
     for num in input_list:
         result += num
     return result
