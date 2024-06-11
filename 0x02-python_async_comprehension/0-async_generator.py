5f02506f020112bca2922a51e0ac3892b038dafa
Youssef <youssefkaramedu@gmail.com>
2024-06-11 20:18:43 +0000

    Add documnetation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -7,6 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[int, None]:
+    ''' function that waits for 1 sec'''
     for i in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
