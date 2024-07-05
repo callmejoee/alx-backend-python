8652e4e9c104faad2046c2c872319d2b84f2ccae
Youssef <youssefkaramedu@gmail.com>
2024-07-05 23:31:19 +0000

    Add class

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -6,6 +6,7 @@ from parameterized import parameterized
 from utils import access_nested_map
 from unittest.mock import patch, Mock
 from utils import get_json
+from utils import memoize
 
 class TestAccessNestedMap(unittest.TestCase):
     @parameterized.expand([
@@ -40,3 +41,24 @@ class TestGetJson(unittest.TestCase):
 
             mocked_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+class TestClass:
+    def a_method(self):
+        return 42
+
+    @memoize
+    def a_property(self):
+        return self.a_method()
+
+class TestMemoize(unittest.TestCase):
+    def test_memoize(self):
+        with patch.object(TestClass, 'a_method', return_value=42) as mocked_method:
+            test_instance = TestClass()
+            
+            result1 = test_instance.a_property
+            result2 = test_instance.a_property
+            
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+            
+            mocked_method.assert_called_once()
