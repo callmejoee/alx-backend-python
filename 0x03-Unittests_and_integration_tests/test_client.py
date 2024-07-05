82b0fe539b242b48d33e2a7c974016f4ec65b169
Youssef <youssefkaramedu@gmail.com>
2024-07-05 23:46:25 +0000

    Add file test_client

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -1,15 +1,29 @@
+#!/usr/bin/env python3
+"""Unit tests for the client module."""
 import unittest
-from unittest.mock import patch
+from typing import Dict
+from unittest.mock import (
+    MagicMock,
+    patch,
+)
 from parameterized import parameterized
+from requests import HTTPError
+
 from client import GithubOrgClient
+from fixtures import TEST_PAYLOAD
+
 
 class TestGithubOrgClient(unittest.TestCase):
+    """Test suite for the GithubOrgClient class."""
+
     @parameterized.expand([
-        ("google", {"login": "google"}),
-        ("abc", {"login": "abc"}),
+        ("google", {'login': "google"}),
+        ("abc", {'login': "abc"}),
     ])
-    @patch('client.GithubOrgClient.get_json', return_value={"login": "expected"})
-    def test_org(self, org_name, expected_json, mock_get_json):
+    @patch("client.get_json")
+    def test_org(self, org_name: str, expected_response: Dict, mock_get_json: MagicMock) -> None:
+        """Ensure that the org method returns the correct response."""
+        mock_get_json.return_value = expected_response
         client = GithubOrgClient(org_name)
-        self.assertEqual(client.org, {"login": "expected"})
+        self.assertEqual(client.org(), expected_response)
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
